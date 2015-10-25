7e82599379c109745828922a5fd12b2140be3593
jplaitin <jplaitin@gmail.com>
2015-10-25 08:57:30 +0200

    ProgressStatusView base new HUD view

---
+++ app/ios/Classes/UI/Components/ProgressStatusView.h
@@ -0,0 +1,13 @@
+//
+//  ProgressStatusView.h
+//  Carat
+//
+//  Created by Jarno Petteri Laitinen on 25/10/15.
+//  Copyright © 2015 University of Helsinki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ProgressStatusView : UIView
+
+@end
---
+++ app/ios/Classes/UI/Components/ProgressStatusView.m
@@ -0,0 +1,21 @@
+//
+//  ProgressStatusView.m
+//  Carat
+//
+//  Created by Jarno Petteri Laitinen on 25/10/15.
+//  Copyright © 2015 University of Helsinki. All rights reserved.
+//
+
+#import "ProgressStatusView.h"
+
+@implementation ProgressStatusView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
