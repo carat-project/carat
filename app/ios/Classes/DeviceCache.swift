fcddbbe2b0c9487b01bf2f479a30d4f867476289
Eemil Lagerspetz <eemil.lagerspetz@gmail.com>
2018-01-29 15:38:55 +0200

    Data model for server-side device list.

---
+++ app/ios/Classes/DeviceCache.swift
@@ -0,0 +1,9 @@
+//
+//  DeviceCache.swift
+//  Carat
+//
+//  Created by Lagerspetz, Eemil H on 29/01/2018.
+//  Copyright © 2018 University of Helsinki. All rights reserved.
+//
+
+import Foundation
