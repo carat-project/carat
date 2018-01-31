c9d766f8f5cf3bd322479f618aed38ca283f34ee
Eemil Lagerspetz <eemil.lagerspetz@gmail.com>
2018-01-31 10:28:58 +0200

    This should not have to be used swift-side.

--- app/ios/Classes/IosDeviceNames.swift
+++ app/ios/Classes/IosDeviceNames.swift
@@ -9,72 +9,79 @@
 import Foundation
 
 class IosDeviceNames: NSObject {
+    
+    //MARK: Class Properties
+
+    // Singleton access
     static let sharedInstance = IosDeviceNames()
-    
+    // How often to re-fetch the list
+    static let THRESH = UInt64(3600*24)
+    // List file location
+    static let url = "http://carat.cs.helsinki.fi/ios-devices.csv"
 
-  //MARK: Properties
+    //MARK: Instance Properties
 
-  static let url = "http://carat.cs.helsinki.fi/ios-devices.csv"
-
-  var cache:DeviceCache? = nil
-  // Need a separate data model class because the last fetch time also needs to come from disk.
-  // [String: String]() // Empty dictionary to be filled
+    // Need a separate data model class because the last fetch time also needs to come from disk.
+    var cache:DeviceCache? = nil
   
-  var uiDev: UIDeviceHardware = UIDeviceHardware()
-    
     //MARK: Initialization
     
     override init() {
         super.init()
         self.cache = loadCache()
-    }
-  
-  // Fetch device list asynchronously to not block main thread.
-  func fetchDeviceListAsync() {
-    // download data from url
-    // split it to lines and parse it using semicolons
-    // fill cache
-    DispatchQueue.main.async {
-      self.fetchDeviceList()
-    }
-  }
-  
-  // Fetch and store device list on disk.
-  private func fetchDeviceList() {
-    let list = try? String(contentsOf: URL(string: IosDeviceNames.url)!)
-    print("Got list", list ?? "nil")
-    if let gotList = list { // Compare with scala: val list = Some(Seq("a;c", "b;d")); list.map{gotList => ... }
-        let lines = gotList.split(separator: "\n")
-        print("Got lines ",lines)
-        var deviceMap = [String: String]()
-        for line in lines {
-          let parts = line.split(separator: ";")
-          if parts.count > 1 {
-            print("Line ", parts[0], " -> ", parts[1])
-            deviceMap[String(parts[0])] = String(parts[1])
-          }
+        if let gotCache = self.cache {
+            // If it's too old, schedule a fetch
+            if gotCache.lastUpdated + IosDeviceNames.THRESH < UInt64(NSDate().timeIntervalSince1970) {
+                fetchDeviceListAsync()
+            }
         }
-        cache = DeviceCache(deviceMap:deviceMap, lastUpdated:UInt64(NSDate().timeIntervalSince1970))
-        saveCache()
     }
-  }
   
-  func getDeviceName() -> String {
-  // If the list in cache is nil or empty, try to loadCache().
-  //TODO: Check how long it has been since last list update,
-  // And update the list if it has been too long.
-    if cache == nil {
-        print("Cache nil, fetching device list over the network.")
-        fetchDeviceList()
+    // Fetch device list asynchronously to not block main thread.
+    func fetchDeviceListAsync() {
+        DispatchQueue.main.async {
+            self.fetchDeviceList()
+        }
     }
-    print("Platform is ", uiDev.platform())
-    let deviceName = cache?.get(platform: uiDev.platform())
-    if deviceName != nil{
-        return deviceName!
-    } else {
-        return uiDev.platform()
+  
+    // Fetch and store device list on disk.
+    private func fetchDeviceList() {
+        let list = try? String(contentsOf: URL(string: IosDeviceNames.url)!)
+        print("Got list", list ?? "nil")
+        if let gotList = list { // Compare with scala: val list = Some(Seq("a;c", "b;d")); list.map{gotList => ... }
+            let lines = gotList.split(separator: "\n")
+            print("Got lines ",lines)
+            var deviceMap = [String: String]()
+            for line in lines {
+                let parts = line.split(separator: ";")
+                if parts.count > 1 {
+                    print("Line ", parts[0], " -> ", parts[1])
+                    deviceMap[String(parts[0])] = String(parts[1])
+                }
+            }
+            cache = DeviceCache(deviceMap:deviceMap, lastUpdated:UInt64(NSDate().timeIntervalSince1970))
+            saveCache()
+        }
     }
-  }
+    
+    // Get the device name matching the given platform.
+    
+    func getDeviceName(platform: String) -> String {
+        // If the list in cache is nil or empty, try to loadCache().
+        //TODO: Check how long it has been since last list update,
+        // And update the list if it has been too long.
+        if cache == nil {
+            print("Cache nil, fetching device list over the network.")
+            fetchDeviceList()
+        }
+        print("Platform is ", platform)
+        let deviceName = cache?.get(platform: platform)
+        if let gotDevice = deviceName {
+            return gotDevice
+        } else {
+            return platform
+        }
+    }
   
     //MARK: Private methods
     
@@ -95,6 +102,4 @@ class IosDeviceNames: NSObject {
         print("Loaded cache from disk: ", c ?? "nil")
         return c
     }
-  
 }
-
